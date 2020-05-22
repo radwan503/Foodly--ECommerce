d5c9e5d1bbb66247f86787bf5a8ae954f539093c
RadwanDev <radwan.ahmed@tigerit.com>
2020-05-22 10:48:36 +0600

    redesign navbar slider why choose favourite food section

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,38 +1,69 @@
 (function ($) {
-"use strict";  
-    //counter js
-     $('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 10000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
+    "use strict";
+
+    $('.navbar .dropdown-item').on('click', function (e) {
+        var $el = $(this).children('.dropdown-toggle');
+        var $parent = $el.offsetParent(".dropdown-menu");
+        $(this).parent("li").toggleClass('open');
+
+        if (!$parent.parent().hasClass('navbar-nav')) {
+            if ($parent.hasClass('show')) {
+                $parent.removeClass('show');
+                $el.next().removeClass('show');
+                $el.next().css({ "top": -999, "left": -999 });
+            } else {
+                $parent.parent().find('.show').removeClass('show');
+                $parent.addClass('show');
+                $el.next().addClass('show');
+                $el.next().css({ "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 });
+            }
+            e.preventDefault();
+            e.stopPropagation();
         }
     });
-});    
-    
-     //carouserl slider brand
-        $('.brand-active').owlCarousel({
-    dots:true,
-    loop:true,
-            autoplay:true,
-   autoplayHoverPause:true,
-    margin:10,
-    nav:false,       
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:7
+
+    $('.navbar .dropdown').on('hidden.bs.dropdown', function () {
+        $(this).find('li.dropdown').removeClass('show open');
+        $(this).find('ul.dropdown-menu').removeClass('show open');
+    });
+
+    //counter js
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+                duration: 10000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+    });
+
+    //carouserl slider brand
+    $('.brand-active').owlCarousel({
+        dots: true,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 7
+            }
         }
-    }
-});
-    
-   
+    });
+
+
+
+
+    $('[data-toggle="tooltip"]').tooltip()
+
 })(jQuery);	
